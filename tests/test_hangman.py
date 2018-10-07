07aa331f4b19de605a9a0f23043c784a718d5b3e
SofiaGruzdeva <gruzdeva@phystech.edu>
2018-10-07 15:14:25 +0300

    Add tests
    Add tests to some func`

---
+++ tests/test_hangman.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[27]:
+
+import pytest
+import hangman
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    (hangman.input_check('word', []), False),
+    (hangman.input_check('w', []), 'w'),
+    (hangman.input_check('W', []), 'w'),
+    (hangman.input_check('123', []), False),
+])
+def test_input_check(test_input, expected):
+    assert test_input == expected
+
+
+@pytest.mark.parametrize("test_input,exp", [
+    (hangman.try_guess('w', ['*'], 'w'), (['w'], True)),
+    (hangman.try_guess('w', ['*', '*'], 'ww'), (['w', 'w'], True)),
+    (hangman.try_guess('g', ['*'], 'w'), (['*'], False))
+])
+def test_try_guess(test_input, exp):
+    assert test_input == exp
