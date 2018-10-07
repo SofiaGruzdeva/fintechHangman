78174ecc3cd19b63ddb4848eaa93485f9e344bf4
SofiaGruzdeva <gruzdeva@phystech.edu>
2018-10-07 15:09:46 +0300

    Fix code style
    Fix code style errors in all files

--- hangman/__init__.py
+++ hangman/__init__.py
@@ -1,6 +1,8 @@
 """Have a nice evening playing this game"""
 import random
 GAME_DICT = ['circle', 'triangle', 'rectangle', 'square', 'trapezoid']
+
+
 def input_check(guess, used):
     """Check input string whatever you can use it in current Hangman game"""
     guess = str.strip(guess)
@@ -15,6 +17,7 @@ def input_check(guess, used):
         return False
     return guess.lower()
 
+
 def try_guess(guess, current_game, game_word):
     """Test input letter"""
     result = False
@@ -24,31 +27,39 @@ def try_guess(guess, current_game, game_word):
             current_game[i] = guess
     return current_game, result
 
-def main():
+
+def game(game_word, max_err=5):
     """The game itself"""
-    game_word = random.choice(GAME_DICT)
     current_game = ['*'] * len(game_word)
     counter = 0
-    max_errors = 5
     used = []
-    while counter < max_errors and '*' in current_game:
+    while counter < max_err and '*' in current_game:
         guess = input_check(input('Guess a letter:\n'), used)
         if guess:
             used.append(guess)
             current_game, result = try_guess(guess, current_game, game_word)
             if not result:
                 counter += 1
-                print('Missed, mistake ', counter, ' out of ', max_errors, '.\n')
+                print('Missed, mistake ', counter, ' out of ', max_err, '.\n')
             else:
                 print('Hit!\n')
         print('The word: ', end='', flush=True)
         for i in current_game:
             print(i, sep=' ', end='', flush=True)
         print('\n')
+    return counter
+
+
+def main():
+    """Enter and finish the game"""
+    game_word = random.choice(GAME_DICT)
+    max_errors = 5
+    counter = game(game_word, max_errors)
     if counter >= max_errors:
         print('You lost!\n')
     else:
         print('You won!\n')
+
 
 if __name__ == '__main__':
     main()
